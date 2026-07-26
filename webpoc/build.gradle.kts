27c5fb1e0c4c74a55927367e433a6b2af763728e
auto-mini <224799543+auto-mini@users.noreply.github.com>
2026-07-26 18:04:44 +0900

    Prepare web callback recovery build

--- webpoc/build.gradle.kts
+++ webpoc/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.local.matholickiosk.webpoc"
         minSdk = 33
         targetSdk = 37
-        versionCode = 33
-        versionName = "0.4.0-rc16"
+        versionCode = 34
+        versionName = "0.4.0-rc17"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
