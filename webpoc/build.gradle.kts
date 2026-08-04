4eee0aa2875360887b2627cc3dada03c852ef3f3
auto-mini <224799543+auto-mini@users.noreply.github.com>
2026-08-04 14:10:48 +0900

    chore(release): prepare web rc130

--- webpoc/build.gradle.kts
+++ webpoc/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.local.matholickiosk.webpoc"
         minSdk = 33
         targetSdk = 37
-        versionCode = 146
-        versionName = "0.4.0-rc129"
+        versionCode = 147
+        versionName = "0.4.0-rc130"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
